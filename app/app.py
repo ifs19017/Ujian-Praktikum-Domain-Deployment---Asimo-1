91768d506900e08bad6051bfb323d8969d4903e2
Montana Gurning <delmontana28@gmail.com>
2022-10-21 22:10:00 +0700

    Update All

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 '''
-	Contoh Deloyment untuk Domain Data Science (DS)
+	Contoh Deloyment untuk Domain Natural Language Processing (NLP)
 	Orbit Future Academy - AI Mastery - KM Batch 3
 	Tim Deployment
 	2022
@@ -10,13 +10,26 @@
 from flask import Flask,render_template,request,jsonify
 import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
 from joblib import load
+import re
+import pickle
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from fungsi import *
 
 # =[Variabel Global]=============================
 
 app   = Flask(__name__, static_url_path='/static')
 model = None
+
+stopwords_ind = None
+key_norm      = None
+factory       = None
+stemmer       = None
+vocab         = None
 
 # =[Routing]=====================================
 
@@ -25,52 +38,54 @@ model = None
 def beranda():
     return render_template('index.html')
 
-# [Routing untuk API]	
+# [Routing untuk API]		
 @app.route("/api/deteksi",methods=['POST'])
 def apiDeteksi():
-	# Nilai default untuk variabel input atau features (X) ke model
-	input_sepal_length = 5.1
-	input_sepal_width  = 3.5
-	input_petal_length = 1.4
-	input_petal_width  = 0.2
+	# Nilai default untuk string input 
+	text_input = ""
 	
 	if request.method=='POST':
-		# Set nilai untuk variabel input atau features (X) berdasarkan input dari pengguna
-		input_sepal_length = float(request.form['sepal_length'])
-		input_sepal_width  = float(request.form['sepal_width'])
-		input_petal_length = float(request.form['petal_length'])
-		input_petal_width  = float(request.form['petal_width'])
+		# Set nilai string input dari pengguna
+		text_input = request.form['data']
 		
-		# Prediksi kelas atau spesies bunga iris berdasarkan data pengukuran yg diberikan pengguna
-		df_test = pd.DataFrame(data={
-			"SepalLengthCm" : [input_sepal_length],
-			"SepalWidthCm"  : [input_sepal_width],
-			"PetalLengthCm" : [input_petal_length],
-			"PetalWidthCm"  : [input_petal_width]
-		})
+		# Text Pre-Processing
+		text_input = text_preprocessing_process(text_input,key_norm,stopwords_ind,stemmer)
 
-		hasil_prediksi = model.predict(df_test[0:1])[0]
+		# TF-IDF
+		tf_idf_vec = TfidfVectorizer(decode_error="replace", vocabulary=set(vocab))
 
-		# Set Path untuk gambar hasil prediksi
-		if hasil_prediksi == 'Iris-setosa':
-			gambar_prediksi = '/static/images/iris_setosa.jpg'
-		elif hasil_prediksi == 'Iris-versicolor':
-			gambar_prediksi = '/static/images/iris_versicolor.jpg'
+		# Prediksi (Penipuan, Promo, atau Normal)
+		hasil = model.predict(tf_idf_vec.fit_transform([text_input]))
+
+		if(hasil==0):
+			hasil_prediksi = "Normal"
+		elif (hasil==1):
+			hasil_prediksi = "Penipuan"
 		else:
-			gambar_prediksi = '/static/images/iris_virginica.jpg'
+			hasil_prediksi = "Promo"
 		
 		# Return hasil prediksi dengan format JSON
 		return jsonify({
-			"prediksi": hasil_prediksi,
-			"gambar_prediksi" : gambar_prediksi
+			"data": hasil_prediksi,
 		})
 
 # =[Main]========================================
 
 if __name__ == '__main__':
 	
+	# Setup
+	stopwords_ind = stopwords.words('indonesian')
+	stopwords_ind = stopwords_ind + more_stopword
+	
+	key_norm = pd.read_csv('key_norm.csv')
+	
+	factory = StemmerFactory()
+	stemmer = factory.create_stemmer()
+	
+	vocab = pickle.load(open('kbest_feature.pickle', 'rb'))
+	
 	# Load model yang telah ditraining
-	model = load('model_iris_dt.model')
+	model = load('model_spam_tfidf_nb.model')
 
 	# Run Flask di localhost 
 	app.run(host="localhost", port=5000, debug=True)
